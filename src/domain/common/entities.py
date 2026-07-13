758555ed9ead854a2bab7faffe6c1b9cdc51b0b7
Eugene Mozge <eumozge@gmail.com>
2026-07-13 18:00:44 +0300

    Revert "refactor: replace imperative mapping with mapped_as_dataclass and TypeDecorator"
    This reverts commit 3f1f496184562a9029817b19d26c75f456687aca.

--- src/domain/common/entities.py
+++ src/domain/common/entities.py
@@ -1,2 +1,5 @@
-class Entity:
+from abc import ABC
+
+
+class Entity(ABC):
     pass
--- src/domain/links/entities.py
+++ src/domain/links/entities.py
@@ -1,25 +1,15 @@
+from dataclasses import dataclass, field
 from datetime import datetime
-from uuid import uuid4
-
-from sqlalchemy.orm import Mapped, mapped_column
 
 from domain.common.entities import Entity
 from domain.links.value_objects.id import ID
 from domain.links.value_objects.url import URL
-from infra.db.models.base import mapper_registry
-from infra.db.models.types import IDColumn, URLColumn
 
 
-@mapper_registry.mapped_as_dataclass
+@dataclass
 class Link(Entity):
-    __tablename__ = "links"
+    id: ID
+    original: URL
+    short: URL
+    created_at: datetime = field(default_factory=datetime.now)
 
-    id: Mapped[ID] = mapped_column(IDColumn, primary_key=True, default_factory=uuid4, init=False)
-    original: Mapped[URL] = mapped_column(URLColumn, nullable=False)
-    short: Mapped[URL] = mapped_column(URLColumn, nullable=False)
-    created_at: Mapped[datetime] = mapped_column(
-        nullable=False,
-        server_default="now()",
-        default_factory=datetime.now,
-        init=False,
-    )
